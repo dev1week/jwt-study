7820fc96746d86765e697bfebe14d0480d61ce0a
dev1week <rondo2860@gmail.com>
2023-03-20 17:43:24 +0900

    #1 -4 jwt쓰기 위해 security 기본설정

---
+++ src/main/java/com/example/demo/Config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.example.demo.Config;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CorsFilter corsFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        //세션을 사용하지 않음
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                //CorsConfig 적용
+                .addFilter(corsFilter)
+                //기본적인 로그인 방식 사용하지 않음
+                .formLogin().disable()
+                //
+                .httpBasic().disable()
+                .authorizeRequests()
+                //권한에 따라 허용할 api 정하기
+                .antMatchers("/api/v1/user/**")
+                .access("hasRole('ROLE_USER')or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/manager/**")
+                .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/admin/**")
+                .access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll();
+    }
+}
