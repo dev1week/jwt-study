a4c23dba0691bd72d456c9e3c08cd0f062053ec8
dev1week <rondo2860@gmail.com>
2023-03-21 12:19:31 +0900

    #1 -9 회원가입 로직 구현

--- src/main/java/com/example/demo/controller/RestApiController.java
+++ src/main/java/com/example/demo/controller/RestApiController.java
@@ -1,10 +1,20 @@
 package com.example.demo.controller;
 
+import com.example.demo.Model.User;
+import com.example.demo.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RestApiController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
     @GetMapping("home")
     public String home(){
         return "<h1>home</h1>";
@@ -13,5 +23,16 @@ public class RestApiController {
     @PostMapping("token")
     public String token(){
         return "<h1>token</h1>";
+    }
+
+
+    @PostMapping("join")
+    public String join(@RequestBody User user){
+        user.setUsername(user.getUsername());
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles("ROLE_USER");
+        userRepository.save(user);
+
+        return "회원가입 완료";
     }
 }
